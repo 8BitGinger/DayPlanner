faa340139026a1c325fb0c59eed966e770779968
8BitGinger <ryan.fann>
2023-09-21 15:18:52 -0500

    Added Light/Dark Mode

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # DayPlanner
+
+Links:
+Fonts: https://fontawesome.com/icons/code?f=classic&s=solid
+
+jQuery/: https://jqueryui.com/
+
+Bootstrap//: https://getbootstrap.com/
+
+Resources Used:
+To figure out how to change the div colors:  https://softauthor.com/change-background-color-javascript/
+
+Used this to work out the JS logic and html structure: https://github.com/EdenKhaos/05-work-day-scheduler   Had to update to use dayJS vs Moment, but was a good guide and notes were well thought out and explanatory.  
+ 
+ To learn Bootstrap classes: https://hackerthemes.com/bootstrap-cheatsheet/#form-control-range
+
+ Tutorials for the Save Logic behind JS and also to learn edit/delete functions: https://www.youtube.com/watch?v=MkESyVB4oUw
+
+Light Mode and Dark Mode help:
+ https://www.geeksforgeeks.org/how-to-create-dark-light-mode-for-website-using-javascript-jquery/
+ 
+ https://www.youtube.com/watch?v=Kxv7GIDK9tg
+
+
+
+
+ What I learned: 
+ What I learned most in this project is how to get the integer split from a string so that I can use that as a value for it's child and make effective changes.  
+ I learned about the Margin:right and Margin:left features to slop only the "end cap" elements to be rounded.  
+ I learned how to use dayJS and and how easy Bootstrap can be for things like jumbotron and cols.  (It made something that would have taken hours in CSS for me to accomplish take a few minutes.)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,88 @@
-$(document).ready(function () {
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-    
+
+
+$(document).ready(function () {//wont start until page has been fully loaded
+    $("#currentDay").text(dayjs().format("dddd MMMM D YYYY")); // use day.js to get the date
+    //this listens for the saveBtn.  Once it does it runs a function
     $(".saveBtn").on("click", function() {
+        //conole.log is for debug only
         console.log(this);
-        var text = $(this).siblings(".event-text").val();
-        var time = $(this).parent().attr("id");
-        console.log(time, text);
+        var text = $(this).siblings(".event-text").val(); // this takes the text from the sibling of the button to save later
+        var time = $(this).parent().attr("id"); // this takes the ID of the hour block as the value
+        console.log(text);
+        console.log(time);
+        //saves both the Time and Text in Local Storage so it will be there upon new page load
         localStorage.setItem(time, text);
     })
 
-    $("#hour-9 .event-text").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .event-text").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .event-text").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .event-text").val(localStorage.getItem("hour-12"));
-    $("#hour-1 .event-text").val(localStorage.getItem("hour-1"));
-    $("#hour-2 .event-text").val(localStorage.getItem("hour-2"));
-    $("#hour-3 .event-text").val(localStorage.getItem("hour-3"));
-    $("#hour-4 .event-text").val(localStorage.getItem("hour-4"));
-    $("#hour-5 .event-text").val(localStorage.getItem("hour-5"));
+        //this checks the val of peviously stored info and populates it if applicable
+    $("#hour9 .event-text").val(localStorage.getItem("hour9"));
+    $("#hour10 .event-text").val(localStorage.getItem("hour10"));
+    $("#hour11 .event-text").val(localStorage.getItem("hour11"));
+    $("#hour12 .event-text").val(localStorage.getItem("hour12"));
+    $("#hour13 .event-text").val(localStorage.getItem("hour13"));
+    $("#hour14 .event-text").val(localStorage.getItem("hour14"));
+    $("#hour15 .event-text").val(localStorage.getItem("hour15"));
+    $("#hour16 .event-text").val(localStorage.getItem("hour16"));
+    $("#hour17 .event-text").val(localStorage.getItem("hour17"));
 
+    // this tracks the current hour and redefines the class of each text area
+    // based on the current, past, present and future
     function hourTracker() {
-        var currentHour=moment().hour();
+        //get current number of hours.
+        var currentHour = dayjs().hour(); // use of dayjs to get the hour of the day
 
+        // loop to check each hour block and then split the hour number off of the id attribute
+        //originally had the numbers assigned with dashes until I realized that was giving them a negativ value
+        $(".hour-block").each(function () {
+            var fixedHour = (parseInt($(this).attr("id").split("hour")[1]))
+            console.log( fixedHour, currentHour)
+
+            //this definies how each class is applied
+            if (fixedHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (fixedHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker(); //re-run function
+})
+
+
+$( ".toggle" ).on("click", function() {
+    if( $( "body" ).hasClass( "darkMode" )) {
+        $( "body" ).removeClass( "darkMode" );
+    } else {
+        $( "body" ).addClass( "darkMode" );
+    }
+
+    if( $( ".jumbotron" ).hasClass( "darkModejumbo" )) {
+        $( ".jumbotron" ).removeClass( "darkModejumbo" );
+    } else {
+        $( ".jumbotron").addClass( "darkModejumbo" );
+    }
+
+    if($( ".hour" ).hasClass( "hourDark")){
+        $( ".hour" ).removeClass( "hourDark" );
+    } else {
+        $( ".hour" ).addClass( "hourDark");
+    }
+        
+    if($( "#toggle" ).hasClass( "toggleDark")){
+        $( "#toggle" ).removeClass( "toggleDark" );
+    } else {
+        $( "#toggle" ).addClass( "toggleDark");
     }
 
 })
+    
